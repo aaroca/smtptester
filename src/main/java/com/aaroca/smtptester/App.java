4b54b31ceaecb5f0ca11ec403a24758f425c3b06
Álvaro Aroca <alvaro.aroca.m@gmail.com>
2021-01-21 18:16:27 +0100

    First commit.

---
+++ src/main/java/com/aaroca/smtptester/App.java
@@ -0,0 +1,12 @@
+package com.aaroca.smtptester;
+
+import java.util.logging.Logger;
+
+public class App {
+
+  private static final Logger LOG = Logger.getLogger(App.class.getName());
+
+  public static void main(String[] args) {
+    LOG.info("App started");
+  }
+}
